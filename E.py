091386c8f0b61eee89cf16071d1b3213665bf8d0
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-02-15 22:31:37 +0800

    Add files via upload

---
+++ E.py
@@ -0,0 +1,13 @@
+def __init__(keyword):
+    """await and async"""
+    'True or False'
+    'def not del'
+
+    'break with continue'
+    'for as while'
+    'nonlocal in global'
+
+    'if else elif'
+    'expect try raise'
+    'yield pass lambda'
+    return keyword
